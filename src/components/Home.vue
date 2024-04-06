b6604fc6c74b683fea7f187f884cb15a4225f004
Volker Janz <volker.janz@innogames.com>
2024-04-06 20:44:29 +0200

    Add buy me a coffee link

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,7 +5,10 @@
       <div class="max-w-screen-md">
         <h1 class="mb-5 text-5xl font-bold">Welcome to the <span class="gemini">Gemini</span> Movie Detectives</h1>
         <p class="mb-5">Test your movie knowledge, powered by <span class="badge badge-info badge-outline">AI</span>!</p>
-        <router-link to="/quiz" tag="button" class="btn">Get started</router-link>
+        <div class="flex flex-wrap gap-4 items-center justify-center">
+          <div><router-link to="/quiz" tag="button" class="btn">Get started</router-link></div>
+          <div><a href="https://www.buymeacoffee.com/vojay" target="_blank"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=vojay&button_colour=191e24&font_colour=ffffff&font_family=Cookie&outline_colour=ffffff&coffee_colour=FFDD00" /></a></div>
+        </div>
       </div>
     </div>
   </div>
