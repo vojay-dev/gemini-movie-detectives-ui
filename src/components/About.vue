0fd0e3d6a960366353a45d48d8c5cc2700f49796
Volker Janz <volker.janz@innogames.com>
2024-04-06 13:22:37 +0200

    Update About description

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,26 +4,28 @@
       <div class="max-w-screen-md">
         <h1 class="mb-5 text-5xl font-bold">About <span class="gemini">Movie Detectives</span></h1>
         <p class="mb-2 text-justify">
-          Gemini Movie Detectives is a learning project to combine the Gemini 1.0 Pro model via VertexAI with up-to-date
-          data about movies from The Movie Database (TMDB) in a fun quiz game.
+          Gemini Movie Detectives is a project aimed at leveraging the power of the Gemini 1.0 Pro model via VertexAI to
+          create an engaging quiz game using the latest movie data from The Movie Database (TMDB).
         </p>
         <h2 class="text-2xl gemini">Backend</h2>
         <p class="mb-2 text-justify">
-          The backend is implemented using FastAPI and Python based on the Retrieval-Augmented Generation methodology to
-          enrich the LLM request with up-to-date metadata. Furthermore it uses Jinja templating to modularize the prompt
-          generation, splitting it into base, personality and data enhancement templates. That way the LLM can generate
-          accurate movie quiz questions.
+          The backend infrastructure is built with FastAPI and Python, employing the Retrieval-Augmented Generation (RAG)
+          methodology to enrich queries with real-time metadata. Utilizing Jinja templating, the backend modularizes
+          prompt generation into base, personality, and data enhancement templates, enabling the generation of accurate
+          and engaging quiz questions.
         </p>
         <h2 class="text-2xl gemini">Frontend</h2>
         <p class="mb-2 text-justify">
-          The frontend uses Vue 3 as the framework with Vite for frontend tooling. Together with daisyUI and Tailwind
-          CSS it delivers a modern UI experience to interact with the backend.
+          The frontend is powered by Vue 3 and Vite, supported by daisyUI and Tailwind CSS for efficient frontend
+          development. Together, these tools provide users with a sleek and modern interface for seamless interaction
+          with the backend.
         </p>
         <h2 class="text-2xl gemini">Conclusion</h2>
         <p class="mb-2 text-justify">
-          The answers to quiz questions are interpreted by the LLM again. That way the LLM decides on the amount of
-          points and even gives fun and individualized responses to the users answers. This shows the potential or using
-          LLM with RAG in game design and development to create individualized experiences.
+          In Movie Detectives, quiz answers are interpreted by the Language Model (LLM) once again, allowing for dynamic
+          scoring and personalized responses. This showcases the potential of integrating LLM with RAG in game design and
+          development, paving the way for truly individualized gaming experiences. Furthermore, it demonstrates the
+          potential for creating engaging quiz trivia or educational games by involving LLM.
         </p>
       </div>
     </div>
