9c423bf55256fb6bb2e7686c8905ccd025821de3
Volker Janz <volker.janz@innogames.com>
2024-04-19 07:42:06 +0200

    Reduce requests by combining limit and stats

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div v-if="loadingStats || loadingLimitStats" class="text-center"><span class="loading loading-lg loading-spinner text-info"></span></div>
-    <div v-else-if="limitStats && stats">
+    <div v-if="loadingStats" class="text-center"><span class="loading loading-lg loading-spinner text-info"></span></div>
+    <div v-else-if="stats">
       <div class="text-center py-8">
         <div class="stats stats-vertical lg:stats-horizontal shadow">
 
@@ -10,7 +10,7 @@
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
             </div>
             <div class="stat-title">Total quiz count</div>
-            <div class="stat-value text-primary">{{ stats.quiz_count_total }}</div>
+            <div class="stat-value text-primary">{{ stats.stats.quiz_count_total }}</div>
           </div>
 
           <div class="stat">
@@ -18,7 +18,7 @@
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
             </div>
             <div class="stat-title">Total points earned</div>
-            <div class="stat-value text-secondary">{{ stats.points_total }}</div>
+            <div class="stat-value text-secondary">{{ stats.stats.points_total }}</div>
           </div>
 
         </div>
@@ -34,23 +34,23 @@
           <tbody>
           <tr>
             <td>Daily Limit</td>
-            <td>{{ limitStats.daily_limit }}</td>
+            <td>{{ stats.limit.daily_limit }}</td>
           </tr>
           <tr>
             <td>Daily Quiz Count</td>
-            <td>{{ limitStats.quiz_count }}</td>
+            <td>{{ stats.limit.quiz_count }}</td>
           </tr>
           <tr>
             <td>Last Reset Time</td>
-            <td>{{ limitStats.last_reset_time }}</td>
+            <td>{{ stats.limit.last_reset_time }}</td>
           </tr>
           <tr>
             <td>Last Reset Date</td>
-            <td>{{ limitStats.last_reset_date }}</td>
+            <td>{{ stats.limit.last_reset_date }}</td>
           </tr>
           <tr>
             <td>Current Date</td>
-            <td>{{ limitStats.current_date }}</td>
+            <td>{{ stats.limit.current_date }}</td>
           </tr>
           </tbody>
         </table>
@@ -67,32 +67,15 @@ export default {
   name: 'Stats',
   data() {
     return {
-      limitStats: null,
       stats: null,
-      loadingLimitStats: false,
       loadingStats: false,
       error: null
     }
   },
   mounted() {
-    this.fetchLimitStats()
     this.fetchStats()
   },
   methods: {
-    async fetchLimitStats() {
-      this.loadingLimitStats = true
-      try {
-        const response = await fetch(`${API_BASE_URI}/limit`, {redirect: 'follow'})
-        if (!response.ok) {
-          throw new Error('Failed to fetch limit stats')
-        }
-        this.limitStats = await response.json()
-      } catch (error) {
-        this.error = error.message
-      } finally {
-        this.loadingLimitStats = false
-      }
-    },
     async fetchStats() {
       this.loadingStats = true
       try {
