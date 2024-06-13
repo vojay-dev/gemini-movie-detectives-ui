7be4e53a508f1641be74a1d40441b2294315935d
Volker Janz <volker.janz@innogames.com>
2024-06-13 19:30:49 +0200

    Improve home screen with background effects

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,26 +1,28 @@
 <template>
-  <div class="hero h-[calc(100vh-68px)]" :style="{ 'background-image': `url(${backgroundImg})`, 'background-position': 'top' }">
-    <div class="hero-overlay bg-opacity-80"></div>
-    <div class="hero-content text-center text-neutral-content">
-      <kinesis-container>
-        <kinesis-element :strength="30">
-          <div class="max-w-screen-md bg-neutral-900 bg-opacity-80 p-5 rounded-2xl">
-            <kinesis-element :strength="20">
-              <h1 class="mb-5 text-5xl font-bold">Welcome to <span class="gemini">Gemini</span> Movie Detectives</h1>
-            </kinesis-element>
-            <kinesis-element :strength="10">
-              <p class="mb-5"><span v-if="user">Hi, <span class="text-primary">{{ user.displayName }}!</span></span> Test your movie knowledge, powered by <span class="badge badge-info badge-outline">AI</span>!</p>
-              <div class="flex flex-wrap gap-4 items-center justify-center">
-                <div><router-link to="/quiz" tag="button" class="btn btn-primary w-48">Get started</router-link></div>
-                <div>
-                  <router-link v-if="user" to="/quiz" tag="button" class="btn btn-success w-48">Your Profile</router-link>
-                  <button v-else tag="button" class="btn btn-secondary w-48" @click="signinRedirect()">Sign in</button>
+  <div ref='vantaRef'>
+    <div class="hero h-[calc(100vh-68px)]">
+      <div class="hero-overlay bg-none"></div>
+      <div class="hero-content text-center text-neutral-content">
+        <kinesis-container>
+          <kinesis-element :strength="30">
+            <div class="max-w-screen-md bg-neutral-900 bg-opacity-50 p-5 rounded-2xl">
+              <kinesis-element :strength="20">
+                <h1 class="mb-5 text-5xl font-bold">Welcome to <span class="gemini">Gemini</span> Movie Detectives</h1>
+              </kinesis-element>
+              <kinesis-element :strength="10">
+                <p class="mb-5"><span v-if="user">Hi, <span class="text-primary">{{ user.displayName }}!</span></span> Test your movie knowledge, powered by <span class="badge badge-info badge-outline">AI</span>!</p>
+                <div class="flex flex-wrap gap-4 items-center justify-center">
+                  <div><router-link to="/quiz" tag="button" class="btn btn-primary w-48">Get started</router-link></div>
+                  <div>
+                    <router-link v-if="user" to="/quiz" tag="button" class="btn btn-success w-48">Your Profile</router-link>
+                    <button v-else tag="button" class="btn btn-secondary w-48" @click="signinRedirect()">Sign in</button>
+                  </div>
                 </div>
-              </div>
-            </kinesis-element>
-          </div>
-        </kinesis-element>
-      </kinesis-container>
+              </kinesis-element>
+            </div>
+          </kinesis-element>
+        </kinesis-container>
+      </div>
     </div>
   </div>
 </template>
@@ -28,9 +30,11 @@
 <script setup>
 import backgroundImg from '../assets/bg-home.webp'
 import {useCurrentUser, useFirebaseAuth} from "vuefire";
-import {onMounted, ref} from "vue";
+import {onBeforeUnmount, onMounted, ref} from "vue";
 import {getRedirectResult, signInWithRedirect} from "firebase/auth";
 import {googleAuthProvider} from "../main.js";
+import HALO from 'vanta/dist/vanta.halo.min'
+import * as THREE from 'three';
 
 const auth = useFirebaseAuth()
 const user = useCurrentUser()
@@ -49,4 +53,21 @@ onMounted(() => {
     error.value = reason
   })
 })
+
+const vantaRef = ref(null);
+let vantaEffect;
+
+onMounted(() => {
+  vantaEffect = HALO({
+    el: vantaRef.value,
+    THREE: THREE,
+    size: 2
+  });
+});
+
+onBeforeUnmount(() => {
+  if (vantaEffect) {
+    vantaEffect.destroy();
+  }
+});
 </script>
