b5532cadda8d865e8feb7b49589834af7f793a19
Volker Janz <volker.janz@innogames.com>
2024-06-16 15:13:53 +0200

    Add home button to menu

--- src/App.vue
+++ src/App.vue
@@ -15,10 +15,10 @@
         </div>
       </div>
       <div class="navbar-center">
-        <a class="btn btn-ghost text-lg gemini">
-          <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24"><path fill="#ffffff" d="M12 20h8v2h-8C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10a9.96 9.96 0 0 1-2 6h-2.708A8 8 0 1 0 12 20m0-10a2 2 0 1 1 0-4a2 2 0 0 1 0 4m-4 4a2 2 0 1 1 0-4a2 2 0 0 1 0 4m8 0a2 2 0 1 1 0-4a2 2 0 0 1 0 4m-4 4a2 2 0 1 1 0-4a2 2 0 0 1 0 4"/></svg>
-          Movie Detectives
-        </a>
+        <router-link to="/" tag="button" class="btn btn-ghost text-lg gemini">
+        <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24"><path fill="#ffffff" d="M12 20h8v2h-8C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10a9.96 9.96 0 0 1-2 6h-2.708A8 8 0 1 0 12 20m0-10a2 2 0 1 1 0-4a2 2 0 0 1 0 4m-4 4a2 2 0 1 1 0-4a2 2 0 0 1 0 4m8 0a2 2 0 1 1 0-4a2 2 0 0 1 0 4m-4 4a2 2 0 1 1 0-4a2 2 0 0 1 0 4"/></svg>
+        Movie Detectives
+        </router-link>
       </div>
       <div class="navbar-end">
         <div class="dropdown dropdown-end">
