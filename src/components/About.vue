62f66b168f2de6b89c232602954d0f89ff8cf3a0
Volker Janz <volker.janz@innogames.com>
2024-07-01 20:27:50 +0200

    Improve VueFlow styling

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="container mx-auto h-[calc(100vh-68px)] pt-5 pb-10">
+    <svg width="0" height="0">
+      <defs>
+        <linearGradient id="gradient-stroke" gradientUnits="objectBoundingBox" gradientTransform="rotate(100)">
+          <stop offset="0%" stop-color="#217BFE"/>
+          <stop offset="27%" stop-color="#078EFB"/>
+          <stop offset="50%" stop-color="#A190FF"/>
+          <stop offset="100%" stop-color="#BD99FE"/>
+        </linearGradient>
+      </defs>
+    </svg>
     <VueFlow :nodes="nodes" :edges="edges" @nodeDragStop="onNodeDragStop">
       <MiniMap maskColor="rgb(7, 14, 29, 0.7)" />
       <Background />
@@ -18,7 +28,7 @@ const nodes = ref([
   {"id":"frontend","position":{"x":856,"y":596},"data":{"label":"Movie Detectives Frontend (VueJS)"},"style":{"backgroundColor": "#4bffc8"}},
   {"id":"gemini","position":{"x":120,"y":206},"data":{"label":"Gemini"},"style":{"backgroundColor": "#4ba4ff"}},
   {"id":"imagen","position":{"x":264,"y":98},"data":{"label":"Imagen"},"style":{"backgroundColor": "#4ba4ff"}},
-  {"id":"tts","position":{"x":542,"y":28},"data":{"label":"Google Text-to-Speech"},"style":{"backgroundColor": "#da87ff"}},
+  {"id":"tts","position":{"x":552,"y":28},"data":{"label":"Google Text-to-Speech"},"style":{"backgroundColor": "#da87ff"}},
   {"id":"firebase","position":{"x":1066,"y":206},"data":{"label":"Firebase"},"style":{"backgroundColor": "#ff7070"}},
   {"id":"wiki","position":{"x":880,"y":98},"data":{"label":"Wikipedia"},"style":{"backgroundColor": "#c3ff6e"}}
 ])
