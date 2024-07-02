5013e9284ed61e89d5a279ffaf01bb2a126ca5d1
Volker Janz <volker.janz@innogames.com>
2024-07-02 13:22:50 +0200

    Adjust custom cursor with smooth motion

--- src/components/CustomCursor.vue
+++ src/components/CustomCursor.vue
@@ -12,31 +12,53 @@ const cursorStyle = ref({
   height: '30px'
 })
 
+let targetX = 0
+let targetY = 0
+let currentX = 0
+let currentY = 0
+
+// linear interpolation to smooth the motion
+function lerp(start, end, factor) {
+  return start + (end - start) * factor
+}
+
+// update the cursor position smoothly
 function updateCursorPosition(e) {
-  requestAnimationFrame(() => {
-    cursorStyle.value.left = `${e.clientX}px`
-    cursorStyle.value.top = `${e.clientY}px`
+  targetX = e.clientX
+  targetY = e.clientY
 
-    const target = e.target
-    const isClickable =
-        target.tagName.toLowerCase() === 'a' ||
-        target.tagName.toLowerCase() === 'button' ||
-        target.closest('a') ||
-        target.closest('button') ||
-        target.dataset.clickable === 'true'
+  const target = e.target
+  const isClickable =
+      target.tagName.toLowerCase() === 'a' ||
+      target.tagName.toLowerCase() === 'button' ||
+      target.closest('a') ||
+      target.closest('button') ||
+      target.dataset.clickable === 'true'
 
-    if (isClickable) {
-      cursorStyle.value.width = '50px'
-      cursorStyle.value.height = '50px'
-    } else {
-      cursorStyle.value.width = '30px'
-      cursorStyle.value.height = '30px'
-    }
-  })
+  if (isClickable) {
+    cursorStyle.value.width = '50px'
+    cursorStyle.value.height = '50px'
+  } else {
+    cursorStyle.value.width = '30px'
+    cursorStyle.value.height = '30px'
+  }
+}
+
+// animate the cursor
+function animateCursor() {
+  currentX = lerp(currentX, targetX, 0.2)
+  currentY = lerp(currentY, targetY, 0.2)
+
+  cursorStyle.value.left = `${currentX}px`
+  cursorStyle.value.top = `${currentY}px`
+
+  // call animateCursor recursively
+  requestAnimationFrame(animateCursor)
 }
 
 onMounted(() => {
   window.addEventListener('mousemove', updateCursorPosition)
+  animateCursor()
 })
 
 onUnmounted(() => {
