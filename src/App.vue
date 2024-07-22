bd302f59db134b31f9ba3694c269a4746b3b24c2
Volker Janz <volker.janz@innogames.com>
2024-07-22 15:34:15 +0200

    Improve menu

--- src/App.vue
+++ src/App.vue
@@ -46,9 +46,24 @@
             </div>
           </div>
           <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-neutral-900 bg-opacity-80 rounded-box w-52">
-            <li v-if="user"><a @click="signOutUser()">Logout</a></li>
-            <li v-else><a @click="signInUser()">Sign in with Google</a></li>
-            <li v-if="user"><router-link tag="link" to="/profile">Profile</router-link></li>
+            <li v-if="user">
+              <a @click="signOutUser()">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24"><path fill="white" d="m17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4z"/></svg>
+                Logout
+              </a>
+            </li>
+            <li v-else>
+              <a @click="signInUser()">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24"><path fill="white" d="M12 21v-2h7V5h-7V3h7q.825 0 1.413.588T21 5v14q0 .825-.587 1.413T19 21zm-2-4l-1.375-1.45l2.55-2.55H3v-2h8.175l-2.55-2.55L10 7l5 5z"/></svg>
+                Sign in with Google
+              </a>
+            </li>
+            <li v-if="user">
+              <router-link tag="link" to="/profile">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24"><path fill="white" fill-rule="evenodd" d="M8 7a4 4 0 1 1 8 0a4 4 0 0 1-8 0m0 6a5 5 0 0 0-5 5a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3a5 5 0 0 0-5-5z" clip-rule="evenodd"/></svg>
+                Profile
+              </router-link>
+            </li>
           </ul>
         </div>
       </div>
